6025782a7b47a51e0532a4cc96fec20f8487df49
muddasir <muddasirjaved92@gmail.com>
2025-05-16 02:33:35 +0500

    app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import  { useState } from 'react'
-import '../src/app.css'
-import Navbar from './components/navbar/Navbar.jsx'
+import '../src/App'
+import Navbar from './components/navbar/Navbar'
 import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/cart/Cart'
