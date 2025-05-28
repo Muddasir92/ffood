7811ad78ac5041d2ca8480e3868b9490adf30615
muddasir <muddasirjaved92@gmail.com>
2025-05-29 00:17:03 +0500

    nav error 12:16am

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import  { useState } from 'react'
 import '../src/App'
-import Navbar from './components/navbar/navbar'
+import Navbar from "./components/navbar/Navbar"
 import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/cart/Cart'
