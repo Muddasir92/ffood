727d81786dbf46e082d51179d4a41c6405646d1f
muddasir <muddasirjaved92@gmail.com>
2025-05-28 22:56:36 +0500

    10:56

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import "./Home.css";
-import Header from '../../components/header/Header.jsx';
+import Header from "../../components/header/Header";
 import Exploremenu from "../../components/exploreMenu/Exploremenu";
 import { useState } from "react";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
